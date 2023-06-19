7c3987fea7aaa7541ba2518891ec1a3224c946fe
Pankaj Singh Chauhan <pschauhan974@gmail.com>
2023-06-19 11:22:25 +0530

    updated

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 const fastify = require('fastify')();
-const { join } = require('path');
-const swaggerUi = require('@fastify/swagger');
-
-const swaggerOptions = {
-  routePrefix: '/docs',
-  swagger: {
-    info: {
-      title: 'Fastify Swagger',
-      description: 'API Documentation using Fastify and Swagger',
-      version: '1.0.0',
-    },
-    externalDocs: {
-      url: 'https://swagger.io',
-      description: 'Find more info here',
-    },
-    host: 'localhost:3000',
-    schemes: ['http'],
-    consumes: ['application/json'],
-    produces: ['application/json'],
-  },
-  exposeRoute: true,
-};
-
-// fastify.register(swaggerUi, swaggerOptions);
 
 fastify.get('/', (request, reply) => {
   reply.send({ message: 'Hello, World!' });
